b15c78f369bface06deb6493e901c739b3991298
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-04-28 13:11:51 +0800

    feat: add spinner and handle disable input

--- src/swap_frontend/src/App.jsx
+++ src/swap_frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// src/swap_frontend/src/App.jsx
 import { memo, useEffect, useState } from 'react';
 import { Principal } from "@dfinity/principal";
 import { canisterId as b23CanisterId } from '../../declarations/b23token';
@@ -6,7 +7,7 @@ import { nns_ledger, idlFactory as nnsLedgerIdlFactory, canisterId as nnsLedgerC
 import { Button } from './components/ui/button';
 import { Card } from './components/ui/card';
 import { useToast } from "./components/ui/use-toast";
-import NumberInput from './components/ui/NumberInput';
+import NumberInput from './components/ui/numberInput';
 
 function App() {
   const NNS_LEDGER_CANISTER_ID = nnsLedgerCanisterId;
@@ -15,7 +16,8 @@ function App() {
   const [isConnected, setIsConnected] = useState(false);
   const { toast } = useToast();
   const [spendAmount, setSpendAmount] = useState(10);
-  
+  const [loading, setLoading] = useState(false);
+
   async function checkThatPlugIsConnected() {
     try {
       const isConnected = await window.ic.plug.isConnected();
@@ -66,12 +68,12 @@ function App() {
       });
     }
   };
-  
+
   async function disconnectPlug() {
-      if (isPlugWalletAvailable()) {
-        // await window.ic.plug.disconnect();
-        setIsConnected(false);
-      }
+    if (isPlugWalletAvailable()) {
+      // await window.ic.plug.disconnect();
+      setIsConnected(false);
+    }
   }
 
   async function importToken() {
@@ -88,27 +90,32 @@ function App() {
   };
 
   async function approveSpend() {
+    setLoading(true);
     const actor = await window.ic.plug.createActor({
       canisterId: NNS_LEDGER_CANISTER_ID,
       interfaceFactory: nnsLedgerIdlFactory,
     })
 
-    const result = await actor.icrc2_approve({
-      fee: [],
-      memo: [],
-      from_subaccount: [],
-      created_at_time: [],
-      amount: spendAmount,
-      expected_allowance: [],
-      expires_at: [],
-      spender: {
-        owner: Principal.fromText(BACKEND_CANISTER_ID),
-        // owner: BACKEND_CANISTER_ID,
-        subaccount: [],
-      }
-    });
-
-    console.log({ result })
+    try {
+      const result = await actor.icrc2_approve({
+        fee: [],
+        memo: [],
+        from_subaccount: [],
+        created_at_time: [],
+        amount: spendAmount,
+        expected_allowance: [],
+        expires_at: [],
+        spender: {
+          owner: Principal.fromText(BACKEND_CANISTER_ID),
+          subaccount: [],
+        }
+      });
+      console.log({ result });
+    } catch (error) {
+      console.error("Error approving spend", error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   async function performSwap() {
@@ -125,12 +132,6 @@ function App() {
   return (
     <main>
       {isConnected ? (
-        // <Card>
-        //   <Button onClick={disconnectPlug} variant="default">Disconnect Plug</Button>
-        //   <Button onClick={approveSpend} variant="default">Approve Spend</Button>
-        //   <Button onClick={performSwap} variant="default">Perform Swap 0.01 ICP</Button>
-        //   <Button onClick={importToken} variant="default">Import Token</Button>
-        // </Card>
         <div className="flex items-center justify-center min-h-screen">
           <Card className="max-w-sm w-full bg-white shadow-lg rounded-lg p-8">
             <h1 className="text-xl font-semibold text-gray-700 text-center">Bridge23 Early Investors</h1>
@@ -142,19 +143,20 @@ function App() {
               min={1}
               max={250}
               onChange={handleSpendAmountChange}
-            />  
-              <p className="text-gray-600 mt-4 text-center">
-                Max buy 250 ICP
-              </p>          
-            <Button onClick={approveSpend} 
-                className="text-white bg-blue-500 hover:bg-blue-600 w-full py-2 rounded"
-                disabled={spendAmount < 1 || spendAmount > 250}
+              disabled={loading}
+            />
+            <p className="text-gray-600 mt-4 text-center">
+              Max buy 250 ICP
+            </p>
+            <Button onClick={approveSpend}
+              className="text-white bg-blue-500 hover:bg-blue-600 w-full py-2 rounded"
+              disabled={spendAmount < 1 || spendAmount > 250 || loading}
             >
-              Approve Spend
+              {loading ? <div className="loader"></div> : 'Approve Spend'}
             </Button>
           </Card>
         </div>
-        
+
       ) : (
         <div className="flex items-center justify-center min-h-screen">
           <Card className="max-w-sm w-full bg-white shadow-lg rounded-lg p-8">
@@ -163,8 +165,8 @@ function App() {
               Dear friend, It's an honor for us to see you as one of the early investors.
             </p>
             <p className="text-gray-500 text-sm mt-1 text-center">
-              Your investment is the seed that grows tomorrow's innovations. 
-              Thank you for being the early champions of change with Bridge23.       
+              Your investment is the seed that grows tomorrow's innovations.
+              Thank you for being the early champions of change with Bridge23.
             </p>
             <div className="mt-8">
               <Button
@@ -179,7 +181,7 @@ function App() {
             </p>
           </Card>
         </div>
-    )}
+      )}
     </main>
   );
 }
--- src/swap_frontend/src/components/ui/numberInput.jsx
+++ src/swap_frontend/src/components/ui/numberInput.jsx
@@ -1,74 +1,83 @@
+// src/components/ui/numberInput.jsx
 import React, { useState } from 'react';
 import './NumberInput.css';
 
-function NumberInput({ initial = 10, min = 1, max = 250, onChange }) {
+function NumberInput({ initial = 10, min = 1, max = 250, onChange, disabled = false }) {
   const [value, setValue] = useState(initial);
 
   const handleIncrement = () => {
-    const newValue = Math.min(value + 1, max);
-    setValue(newValue);
-    onChange(newValue);
+    if (!disabled) {
+      const newValue = Math.min(value + 1, max);
+      setValue(newValue);
+      onChange(newValue);
+    }
   };
 
   const handleDecrement = () => {
-    const newValue = Math.max(value - 1, min);
-    setValue(newValue);
-    onChange(newValue);
+    if (!disabled) {
+      const newValue = Math.max(value - 1, min);
+      setValue(newValue);
+      onChange(newValue);
+    }
   };
 
   const handleChange = (e) => {
-    const intValue = parseInt(e.target.value, 10);
-    if (!isNaN(intValue)) {
-      setValue(intValue);
-    } else {
-      setValue('');
+    if (!disabled) {
+      const intValue = parseInt(e.target.value, 10);
+      if (!isNaN(intValue)) {
+        setValue(intValue);
+      } else {
+        setValue('');
+      }
     }
   };
 
   const handleBlur = () => {
-    if (value < min) {
-      setValue(min);
-      onChange(min);
-    } else if (value > max) {
-      setValue(max);
-      onChange(max);
-    } else {
-      onChange(value);
+    if (!disabled) {
+      if (value < min) {
+        setValue(min);
+        onChange(min);
+      } else if (value > max) {
+        setValue(max);
+        onChange(max);
+      } else {
+        onChange(value);
+      }
     }
   };
 
   return (
     <div className='my-3 px-8'>
-    <div className="flex items-center justify-center space-x-2 border-2 border-gray-800 rounded-full">
-        <button 
-            className="bg-gray-800 text-white px-7 py-2 rounded-l-full hover:bg-gray-900"        
-            onClick={handleDecrement} disabled={value <= min}
+      <div className="flex items-center justify-center space-x-2 border-2 border-gray-800 rounded-full">
+        <button
+          className="bg-gray-800 text-white px-7 py-2 rounded-l-full hover:bg-gray-900"
+          onClick={handleDecrement} disabled={disabled || value <= min}
         >
-            -
+          -
         </button>
         <div className="relative flex-grow ">
-            <input 
-                type="number"
-                className="w-full text-center px-2 py-1 focus:outline-none rounded"
-                value={value} 
-                onChange={handleChange} 
-                onBlur={handleBlur} 
-                style={{ paddingRight: '2rem' }}
-            />
-            <span className="absolute inset-y-0 right-0 px-1 flex items-center text-gray-700">
+          <input
+            type="number"
+            className="w-full text-center px-2 py-1 focus:outline-none rounded"
+            value={value}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            disabled={disabled}
+            style={{ paddingRight: '2rem' }}
+          />
+          <span className="absolute inset-y-0 right-0 px-1 flex items-center text-gray-700">
             ICP
-            </span>
+          </span>
         </div>
-        <button 
-            className="bg-gray-800 text-white px-7 py-2 rounded-r-full hover:bg-gray-900"
-            onClick={handleIncrement} 
-            disabled={value >= max}
+        <button
+          className="bg-gray-800 text-white px-7 py-2 rounded-r-full hover:bg-gray-900"
+          onClick={handleIncrement}
+          disabled={disabled || value >= max}
         >
-            +
+          +
         </button>
+      </div>
     </div>
-    </div>
-
   );
 }
 
